10b0bab9f1fbd623b78fefd66593b1415b71918b
LeoBP1987 <leonardobp1987@gmail.com>
2025-03-24 00:45:24 -0300

    Fix: Ajustando CORS para acesso do FrontEnd

--- setup/settings.py
+++ setup/settings.py
@@ -175,3 +175,8 @@ AUTHENTICATION_BACKENDS = (
     'oauth2_provider.backends.OAuth2Backend',
     'django.contrib.auth.backends.ModelBackend',
 )
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
