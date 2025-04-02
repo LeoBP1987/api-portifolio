98ab6b58268d4e6fbb2a0d6d3695b71feeb1e728
LeoBP1987 <leonardobp1987@gmail.com>
2025-04-02 19:55:06 -0300

    Fix: Retornando campos de descrições longas, backend e frontend de maneira separada no modelo projetos

--- portifolio/serializers.py
+++ portifolio/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from portifolio.models import UsuariosCustomizados, Tecnologias, Projetos
-import json
 
 class UsuariosCustomizadosSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,14 +15,3 @@ class ProjetosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projetos
         fields = '__all__'
-
-    def get_descricoes_conteudo(self, obj):
-        
-        if obj.descricoes and obj.descricoes.path:
-            try:
-                with open(obj.descricoes.path, 'r', encoding='utf-8') as file:
-                    return json.load(file)
-            except Exception as e:
-                print(f"Erro ao ler o arquivo descricoes: {e}")
-                return None
-        return None
