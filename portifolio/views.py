874649890e508b3bfe2b8a702bf8d5a4951ff89b
LeoBP1987 <leonardobp1987@gmail.com>
2025-04-04 20:12:05 -0300

    Fix: Corrigindo email destinatario

--- portifolio/views.py
+++ portifolio/views.py
@@ -91,7 +91,7 @@ def enviar_contato(request):
         {data['mensagem']}
         """
         remetente = data['email']
-        destinatario = ['leonardobp1987@gmail.com']
+        destinatario = [settings.EMAIL_HOST_USER]
         
         send_mail(
             assunto,
